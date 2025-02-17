c4f9ed46457587841f272831c2b80841cd026153
bcombes <puddycomb@gmail.com>
2025-02-17 17:16:47 -0500

    final update

--- server.py
+++ server.py
@@ -1,28 +1,39 @@
+'''
+    Here's the module doc string
+'''
 from flask import Flask, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
-text_response = 'For the given statement, the system response is '
+TEXT_RESPONSE = 'For the given statement, the system response is '
 
 
 @app.route('/emotionDetector', methods=['POST'])
 def detect_emotion():
+    '''
+    This is the http end-point for doc string control
+    '''
 
     # Get JSON data from request
     data = request.get_json()
 
     response = emotion_detector(data['text'])
 
-    text = text_response
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!", 400
+
+    text = TEXT_RESPONSE
+    dominant_emotion = None
+    end_template = f". The dominant emotion is {dominant_emotion}"
 
     for emotion, value in response.items():
-        if(emotion != 'dominant_emotion'):
-            text += "\'{emotion}\': {value},".format(emotion=emotion, value=value)
+        if emotion != 'dominant_emotion':
+            text += f"\'{emotion}\': {value},".format(emotion=emotion, value=value)
 
-    text += ". The dominant emotion is {dominant_emotion}".format(dominant_emotion=response['dominant_emotion'])
+    text += end_template.format(dominant_emotion=response['dominant_emotion'])
 
-    return text       
+    return text
 
 if __name__ == '__main__':
     app.run(debug=True)
